f21d92c391d627efe145c96e6d3a3fe6deb9b3d7
Connor Semb <polishmustard710@gmail.com>
2022-05-24 20:47:30 -0500

    installed dependencies and got port to function/listen

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,20 @@
 const butInstall = document.getElementById('buttonInstall');
-
+const textHeader = document.getElementById('textHeader');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.defferedPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+    
+    butInstall.addEventListener('click', async () => {
+    event.prompt();
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed!';
+    })
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
-
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// TODO: Add a handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    textHeader.textContent = 'Successfully installed!';
+    console.log('👍', 'appinstalled', event);
+});
