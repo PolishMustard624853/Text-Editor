29798a2a0a607d7ac33ad1c0ff99abfeb578976f
Connor Semb <polishmustard710@gmail.com>
2022-05-24 19:48:28 -0500

    database.js logic to method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,4 @@
+import { request } from 'express';
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -13,9 +14,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('GET from the database');
+  const jateDb = openDB('jate', 1);
+  const jx = jateDb.transaction('jate', 'readwrite');
+  const store = jx.objectStore('jate');
+  const request = store.put({ jate: content});
+  const result = await request;
+
+  console.log('data saved database', result);
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from database');
+  const jateDb = await openDB('jate', 1);
+  const jx = jateDb.transaction('jate', 'readwrite');
+  const store = jx.objectStore('jate');
+  const request = store.get(id);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
