a644639e02827d4ecbee18565019b8d3beba8194
Connor Semb <polishmustard710@gmail.com>
2022-05-24 19:12:47 -0500

    aset caching in src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,14 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['script', 'style', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
